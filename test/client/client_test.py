ce644fc3d4c878b7822fb9dc79661898f6f6d3bb
rlnunes <ruanrln@gmail.com>
2016-08-07 23:46:02 -0300

    adding roadmap for the tests

--- test/client/client_test.py
+++ test/client/client_test.py
@@ -1,5 +1,12 @@
 import gpg_group_chat.client
 
+#to test:
+#- open a connection to an existent server
+#- try toopen a connection to an inexistent server
+#- try to open a connection without network connection
+#- send a message to te server and receive response
+#- receive message from the server
 
 def test_start_gpg_groupd_chat():
     assert True
+
